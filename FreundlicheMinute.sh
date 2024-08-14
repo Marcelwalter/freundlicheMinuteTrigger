75ee3e48a56aae539c45acb5eadb7edc5dc3074e
Marcel Walter <marcel@marcels-air.speedport.ip>
2024-08-14 18:28:08 +0200

    Companionscript hinzugefügt

--- FreundlicheMinute.sh
+++ FreundlicheMinute.sh
@@ -1,13 +1,19 @@
 #!/bin/zsh
 
 # Pfad des aktuellen Shell-Skripts ermitteln
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" &> /dev/null && pwd)
 
 # Aktiviert die virtuelle Umgebung
-source "$SCRIPT_DIR/venv/bin/activate"
+source "$SCRIPT_DIR/.venv/bin/activate"
 
 # Führt das Python-Skript mit den angegebenen Parametern aus
-python "$SCRIPT_DIR/script.py" "$@"
+python "$SCRIPT_DIR/main.py" "$@"
 
-# Deactivate the virtual environment
+# Deaktiviert die virtuelle Umgebung
 deactivate
+
+# Terminal-Fenster schließen
+exit
+
+### Mit folgender Expression kann in Companion das Script ausgeführt werden:
+### osascript -e 'tell application "Terminal" to do script "~/Desktop/FreundlicheMinute/FreundlicheMinute.sh 10:28; exit"'
