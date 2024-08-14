cb1f79acafaad12ca5ead6f51ad96d724743d4c2
Marcel Walter <marcel@marcels-air.speedport.ip>
2024-08-14 17:13:41 +0200

    Pfade im Shellscript angepasst

--- FreundlicheMinute.sh
+++ FreundlicheMinute.sh
@@ -1,9 +1,13 @@
 #!/bin/zsh
-# Activate the virtual environment
-source ./.venv/bin/activate
 
-# Run the Python script with the specified time
-python main.py "$@"
+# Pfad des aktuellen Shell-Skripts ermitteln
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Aktiviert die virtuelle Umgebung
+source "$SCRIPT_DIR/venv/bin/activate"
+
+# Führt das Python-Skript mit den angegebenen Parametern aus
+python "$SCRIPT_DIR/script.py" "$@"
 
 # Deactivate the virtual environment
 deactivate
